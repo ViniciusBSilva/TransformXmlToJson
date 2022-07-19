23729e3a18ec549d81b87510b52544f915586e41
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-07-18 23:25:53 -0300

    Development finished

--- TransformXMLandDownload.js
+++ TransformXMLandDownload.js
@@ -1,56 +1,122 @@
 function transformAndDownload() {
 
-    const originalXMLPath = document.getElementById("SourceFile").value;
-    const transformXSLPath = document.getElementById("XSLFile").value;
+    const sourceXmlPath = document.getElementById("SourceFile").value;
+    const sourceXml = loadXMLDoc(sourceXmlPath);
 
-    const originalXML = loadXMLDoc(originalXMLPath);
-    const transformXSL = loadXMLDoc(transformXSLPath);
+    const resultNode = getResultNodeContent(sourceXml);
+    console.log("resultNode", resultNode);
 
-    // code for Chrome, Firefox, Opera, etc.
-    if (document.implementation && document.implementation.createDocument) {
+    const dataNode = getDataNodeContent(sourceXml);
 
-        const xsltProcessor = new XSLTProcessor();
-        xsltProcessor.importStylesheet(transformXSL);
 
-        // const resultDocument = xsltProcessor.transformToFragment(originalXML, document);
-        const resultDocument = xsltProcessor.transformToDocument(originalXML);
+    const output = {
+        ...resultNode,
+        ...dataNode
+    }
+    console.log(output);
 
-        const resultString = new XMLSerializer().serializeToString(resultDocument.documentElement);
+    const resultString = JSON.stringify(output);
 
-        const downloadFile = document.getElementById("DownloadFile").checked;
+    if (document.getElementById("DownloadFile").checked) {
 
-        if (downloadFile) {
+        downloadFile(resultString);
 
-            downloadXML(resultString);
-
-        } else {
-            // Test only
-            const elPreview = document.getElementById("Preview").style.display = "";
-            const elResult = document.getElementById("Result").value = resultString;
-        }
-
+    } else {
+        // Test only
+        const elPreview = document.getElementById("Preview").style.display = "";
+        const elResult = document.getElementById("Output").value = resultString;
     }
 
 }
 
-function downloadXML(stringContent) {
+function getDataNodeContent(sourceXml) {
+
+    const columnNames = getDataColumnNames(sourceXml);
+
+    let dataNode = {
+        "Data": []
+    };
+
+    const rowNodes = [...sourceXml.getElementsByTagName("Row")];
+
+    rowNodes.forEach(row => {
+
+        let dataNodeRow = {};
+        let i = 0;
+        row.childNodes.forEach(child => {
+
+            if (child.nodeType === Node.ELEMENT_NODE) {
+
+                dataNodeRow = {
+                    ...dataNodeRow,
+                    [columnNames[i]]: child.textContent,
+                }
+                i++;
+            }
+
+        });
+
+        dataNode["Data"].push(dataNodeRow);
+
+    });
+
+    return dataNode;
+}
+
+function getDataColumnNames(sourceXml) {
+    let columnNames = {};
+    let i = 0;
+    sourceXml.getElementsByTagName("ColumnNames")[0].childNodes.forEach(element => {
+
+        if (element.nodeType === Node.ELEMENT_NODE) {
+            columnNames = {
+                ...columnNames,
+                [i++]: element.textContent.replace(/\s+/g, '')
+            }
+        }
+    });
+    return columnNames;
+}
+
+function getResultNodeContent(sourceXml) {
+
+    let resultJson = {
+        "Result": {}
+    };
+
+    sourceXml.getElementsByTagName("Result")[0].childNodes.forEach(element => {
+
+        if (element.nodeType === Node.ELEMENT_NODE) {
+            resultJson["Result"] =
+            {
+                ...resultJson["Result"],
+                [element.nodeName]: element.textContent
+            };
+        }
+
+    });
+
+    return resultJson;
+}
+
+function downloadFile(stringContent) {
 
     const targetFilename = document.getElementById("TargetFile").value;
     download(targetFilename, stringContent);
 
 }
 
-function download( filename, stringContent ) {
-    var element = document.createElement( "a" );
+function download(filename, stringContent) {
+    var element = document.createElement("a");
     element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(stringContent));
     element.setAttribute("download", filename);
 
     element.style.display = "none";
-    document.body.appendChild( element );
+    document.body.appendChild(element);
 
     element.click();
 
-    document.body.removeChild( element );
+    document.body.removeChild(element);
 
 }
 
