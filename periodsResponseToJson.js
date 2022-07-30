aad13f17e4f4505dab9ab0eabfad01f93a2984e1
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-07-30 19:10:26 -0300

    Created Expected results, Logic to read Time Ranges (TODO: Adjust output formats

--- periodsResponseToJson.js
+++ periodsResponseToJson.js
@@ -1,15 +1,63 @@
 function periodsResponseToJson(apiResponse) {
 
-    // const resultNode = getResultNodeContent(apiResponse);
-    // const dataNode = getDataNodeContent(apiResponse);
+    const periods = getPeriods(apiResponse);
 
-    // const jsonOutput = {
-    //     ...resultNode,
-    //     ...dataNode
-    // }
+    return periods;
 
-    // return jsonOutput;
+}
 
-    //TODO: handle periods response
-    return '';
+function getPeriods(apiResponse) {
+
+    const periodNodes = [...apiResponse.getElementsByTagName("Period")];
+
+    const periods = {
+        "Periods": []
+    };
+
+    periodNodes.forEach(periodNode => {
+
+        const period = {
+            "Period": readChildren(periodNode.children)
+        }
+        periods["Periods"].push(period);
+
+    });
+
+    return periods;
+}
+
+function readChildren(nodeCollection) {
+
+    console.log("nodeCollection", nodeCollection);
+    console.log("nodeCollection.length", nodeCollection.length);
+
+    const childrenArray = [];
+
+    if (nodeCollection.length > 0) {
+
+        [...nodeCollection].forEach(child => {
+            console.log("child", child);
+
+            if (child.children.length > 0) {
+                const children = readChildren(child.children);
+                childrenArray.push({
+                    [child.nodeName]: children
+                });
+            } else {
+                childrenArray.push({
+                    [child.nodeName]: child.textContent
+                });
+            }
+        });
+
+    } else {
+
+        childrenArray.push({
+            [nodeCollection.nodeName]: nodeCollection.textContent
+        });
+
+    }
+
+    return childrenArray;
+
 }
